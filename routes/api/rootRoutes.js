216234e2c652cfd95d996558ea46565da965b579
suhas-scrobits <suhaskhobragade.scrobits@gmail.com>
2023-07-15 23:48:20 +0530

    test: just checking cookies are being set or not

--- routes/api/rootRoutes.js
+++ routes/api/rootRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
+  const cookies = req.cookies;
+  console.info({ cookies });
+
   res.send("Hello World!");
 });
 
